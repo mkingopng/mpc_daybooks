4fc67a214aa52a3838582e53309a821e6931e168
mkingopng <mkingo@mac.com>
2021-07-22 18:28:59 +1000

    trying to make this work

--- mpc_daybooks_4.py
+++ mpc_daybooks_4.py
@@ -18,93 +18,57 @@ process:
             - OR remove all rows which have a non-numeric value in column 'ACC' index[2]
         - remove the mid & summary sections
         - append the clean data to a clean df
-    - export clean data to csv or xlsx
+    - export clean data to csv or xlsx (complete to this point)
+
+    - move the '-' symbol to the front of the string in column 'AMOUNT'
+
     - automate the analysis. Probably need to use plotly scatter chart. Need hovertext.
         pivot
         scatterplot with hovertext to pick outliers (work orders with variances)
 
 """
 import pandas as pd
-import numpy as np
-import os
-import xlsxwriter
+import glob
 
-# specify the directory
-# directory_in_str = '/home/michaelkingston/Documents/GitHub/mpc_daybooks/07_JUL_2020/'
-directory_in_str = '/Users/michaelkingston/PycharmProjects/MPC_daybooks/07_JUL_2020/'
-directory = os.fsencode(directory_in_str)
-list2 = []
-error_message = ' is throwing an error'
-
-# filename = 'WIP_JNL_MPC_TRN_562310_020720.txt'
+file = 'result.txt'
 
 
-def read_mpc_files():
-    mpc_df = pd.read_fwf(infile, colspecs=[(0, 9), (10, 18), (19, 25), (26, 28), (33, 40), (56, 67), (68, 92),
+def read_mpc_txt_files():
+    read_files = glob.glob('/Users/michaelkingston/PycharmProjects/MPC_daybooks/07_JUL_2020/*.txt')
+    with open('result.txt', 'wb') as outfile:
+        for f in read_files:
+            with open(f, 'rb') as infile:
+                outfile.write(infile.read())
+    return outfile
+
+
+def create_df(file):
+    mpc_df = pd.read_fwf(file, colspecs=[(0, 9), (10, 18), (19, 25), (26, 28), (33, 40), (56, 67), (68, 92),
                                          (93, 100), (101, 111)],
                          names=['TRANS-', 'TRANSDAT', 'ACC', 'CC', 'PRD', 'AMOUNT', 'TEXT', 'WORK.C', 'WO-NO'],
                          dtype={'TRANS-': str, 'TRANSDAT': object, 'ACC': str, 'CC': str, 'PRD': str,
                                 'AMOUNT': str, 'TEXT': str, 'WORK.C': str, 'WO-NO': str},
                          # remove unneeded data from header of first page
                          header=4, skiprows=1)
-
     return mpc_df
 
 
 def clean_mpc_data(mpc_df):
+    # list of all the values in column 'ACC' that i want to keep
     accounts_list = ['001049', '001021', '100112', '350000', '400001', '400916',
                      '400100', '400902', '400903', '400910', '400911', '400912']
-
     # remove all the rows with NaN in 'ACC'
     cleaner_mpc_df1 = mpc_df[mpc_df['ACC'].notna()]
-
     # check to see if the value in each row for column 'ACC' is in the accounts list
     clean_mpc_df = cleaner_mpc_df1[cleaner_mpc_df1['ACC'].isin(accounts_list)]
-
-    # print(clean_mpc_df)
+    for row in clean_mpc_df['ACC']:
+        if clean_mpc_df.loc[clean_mpc_df['AMOUNT'].str.endswith('-'):
+            lambda x: x[-1] + x[:-1]
     return clean_mpc_df
 
 
-# this isn't working
-def append_df(clean_mpc_df):
-    july_df = pd.DataFrame()
-    # this seems to only append the data for the current iteration. it seems to overwrites the previous append.
-    # july_df = july_df.append(clean_mpc_df, ignore_index=True)
-    # this doesn't seem any better
-    july_df = pd.concat([july_df, clean_mpc_df], join='outer')
-    print(july_df)
-    return july_df
-
-
-def export_df_to_xlsx(july_df):
-    # export the cleaned data to .csv or .xlsx
-    july_df.to_excel(r'july_mpc.xlsx', index=False)
-
-
 if __name__ == '__main__':
-    for filename in os.listdir(directory_in_str):
-        with open(directory_in_str + filename) as infile:
-            try:
-                print(filename)
-                next(infile)
-                mpc_df = read_mpc_files()
-                clean_mpc_df = clean_mpc_data(mpc_df)
-                july_df = append_df(clean_mpc_df)
-                export_df_to_xlsx(july_df)
-            except:
-                print(f"{filename} is throwing an error")
-    print('DONE!!!')
-    print(july_df)
-
-    # for filename in os.listdir(directory):
-        # if filename.endswith(".txt"):
-            # do smth
-            # continue
-        # else:
-            # continue
-
-    # this works for one file without the loop, so the basic functions work ok
-    # mpc_df = read_mpc_files()
-    # clean_mpc_df = clean_mpc_data(mpc_df)
-    # july_df = append_df(clean_mpc_df)
-    # export_df_to_xlsx(july_df)
+    outfile = read_mpc_txt_files()
+    mpc_df = create_df(file)
+    clean_mpc_df = clean_mpc_data(mpc_df)
+    clean_mpc_df.to_csv(r'july_mpc.csv')
